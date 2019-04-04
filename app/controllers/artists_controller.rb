6b7203cbebc66a60c855ba646cd00650d5023958
agatheblues <agatheblues@users.noreply.github.com>
2019-04-04 21:17:30 +0200

    Add the artist controller and routes

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,2 +1,22 @@
 class ArtistsController < ApplicationController
+  def index
+    
+  end
+
+  def create
+  
+  end
+
+  def show
+
+  end
+
+  def update
+    
+  end
+
+  def destroy
+
+  end
+
 end
