b44f8968ea02f3542047f9f83bda74d3ca434e82
agatheblues <agatheblues@users.noreply.github.com>
2019-04-07 18:40:41 +0200

    Update album and artist sources models to include 'belongs_to' clause

--- app/models/album_source.rb
+++ app/models/album_source.rb
@@ -1,4 +1,6 @@
 class AlbumSource < ApplicationRecord
+  belongs_to :album
+
   # validations
   validates_presence_of :source_id, :source
 end
--- app/models/artist_source.rb
+++ app/models/artist_source.rb
@@ -1,4 +1,6 @@
 class ArtistSource < ApplicationRecord
+  belongs_to :artist
+  
   # validations
   validates_presence_of :source_id, :source
 end
