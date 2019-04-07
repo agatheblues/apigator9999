599404edd400f64614dabbc3899392f4ec9b75ef
agatheblues <agatheblues@users.noreply.github.com>
2019-04-07 18:40:07 +0200

    Define routes for albums, artists and artist's albums

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,18 @@
 Rails.application.routes.draw do
-  resources :artists, only: [:index, :create, :show, :update, :destroy]
-  resources :albums, only: [:index, :create, :show, :update, :destroy]
+  # ARTISTS
+  get '/artists', to: 'artists#index'
+  post '/artists', to: 'artists#create'
+  get '/artists/:id', to: 'artists#show'
+  put '/artists/:id', to: 'artists#update'
+  delete '/artists/:id', to: 'artists#destroy'
+
+  # ALBUMS
+  get '/albums', to: 'albums#index'
+  post '/albums', to: 'albums#create'
+  get '/albums/:id', to: 'albums#show'
+  put '/albums/:id', to: 'albums#update'
+  delete '/albums/:id', to: 'albums#destroy'
+
+  # ARTIST'S ALBUMS
+  get '/artists/:artist_id/albums', to: 'artist_albums#index'
 end
