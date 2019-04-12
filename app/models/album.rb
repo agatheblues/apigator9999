b56232931270982f94f677d540d0b9eed733075a
Agathe L <lenclen.agathe@gmail.com>
2019-04-12 21:35:34 +0200

    Update artist and album models (remove sources models)

--- app/models/album.rb
+++ app/models/album.rb
@@ -2,13 +2,11 @@ class Album < ApplicationRecord
   # model association
   has_and_belongs_to_many :genres
   has_and_belongs_to_many :artists
-  has_many :album_sources, dependent: :destroy
 
   # nested attributes
   accepts_nested_attributes_for :artists
-  accepts_nested_attributes_for :album_sources
 
   # validations
   validates_presence_of :name, :added_at, :release_date, :total_tracks, :img_url, :height, :width
-  validates_presence_of :album_sources, :artists, :on => :create
+  validates_presence_of :artists, :on => :create
 end
--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,6 +1,5 @@
 class Artist < ApplicationRecord
   # model association
-  has_many :artist_sources, dependent: :destroy
   has_and_belongs_to_many :albums
   
   # validations
