f6df131095c817798dd91d835d2f7938f44d3ca9
Agathe L <lenclen.agathe@gmail.com>
2019-04-16 22:41:06 +0200

    Add albums as nested attributes for artists

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -2,6 +2,9 @@ class Artist < ApplicationRecord
   # model association
   has_and_belongs_to_many :albums
   
+  # nested attributes
+  accepts_nested_attributes_for :albums
+
   # validations
   validates_presence_of :name, :img_url
 end
