5b9628cbf1f5734b670e71b4e3e54b43b6adcffa
Agathe L <lenclen.agathe@gmail.com>
2019-04-17 21:31:08 +0200

    Validate that a source does not exist yet when adding an album

--- app/models/album.rb
+++ app/models/album.rb
@@ -10,13 +10,21 @@ class Album < ApplicationRecord
   validates_presence_of :name, :added_at, :release_date, :total_tracks, :img_url, :height, :width
   # validates_presence_of :artists, :on => :create
 
-  validate :at_least_one_source_exists
+  validate :at_least_one_source_exists, :sources_are_unique
 
   private 
 
   def at_least_one_source_exists
     if (!self[:spotify_id].present? && !self[:discogs_id].present?) 
       errors.add(:base, "either spotify_id or discogs_id must be present")
+    end
+  end
+
+  def sources_are_unique
+    if (self[:spotify_id] && Album.exists?(:spotify_id => self[:spotify_id]))
+      errors.add(:base, "this spotify_id already exists in the database")
+    elsif (self[:discogs_id] && Album.exists?(:discogs_id => self[:discogs_id]))
+      errors.add(:base, "this discogs_id already exists in the database")
     end
   end
 
