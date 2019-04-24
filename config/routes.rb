9c617412badfd4c8518023aff755adc444814976
Agathe L <lenclen.agathe@gmail.com>
2019-04-24 22:51:33 +0200

    Fix linting + switch PUT to PATCH in album routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,15 @@
 Rails.application.routes.draw do
   # ARTISTS
-  get '/artists', to: 'artists#index'
-  post '/artists', to: 'artists#create'
-  get '/artists/:id', to: 'artists#show'
-  put '/artists/:id', to: 'artists#update'
-  delete '/artists/:id', to: 'artists#destroy'
+  get "/artists", to: "artists#index"
+  post "/artists", to: "artists#create"
+  get "/artists/:id", to: "artists#show"
+  put "/artists/:id", to: "artists#update"
+  delete "/artists/:id", to: "artists#destroy"
 
   # ALBUMS
-  get '/albums', to: 'albums#index'
-  post '/albums', to: 'albums#create'
-  get '/albums/:id', to: 'albums#show'
-  put '/albums/:id', to: 'albums#update'
-  delete '/albums/:id', to: 'albums#destroy'
+  get "/albums", to: "albums#index"
+  post "/albums", to: "albums#create"
+  get "/albums/:id", to: "albums#show"
+  patch "/albums/:id", to: "albums#update"
+  delete "/albums/:id", to: "albums#destroy"
 end
