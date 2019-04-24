3e5d4d5d8b01e9434151cad2ee214ea1e4229d63
Agathe L <lenclen.agathe@gmail.com>
2019-04-24 22:31:10 +0200

    No need to manually create the json response because as_json has been overriden

--- app/controllers/albums_controller.rb
+++ app/controllers/albums_controller.rb
@@ -1,6 +1,6 @@
 class AlbumsController < ApplicationController
   before_action :set_album, only: [:show, :update, :destroy]
-  
+
   def index
     @albums = Album.all
     json_response(@albums)
@@ -9,9 +9,9 @@ class AlbumsController < ApplicationController
   def create
     @album = Album.new(album_params)
     if @album.save
-      json_response({:album => @album, :artists => @album.artists}, :created)
+      json_response(@album, :created)
     else
-      json_response({ :message => @album.errors.messages}, :bad_request)
+      json_response({ message: @album.errors.messages }, :bad_request)
     end
   end
 
@@ -33,13 +33,13 @@ class AlbumsController < ApplicationController
 
   def album_params
     params.permit(
-      :name, :added_at, :release_date, :height, :width, :img_url, :total_tracks, :spotify_id, :discogs_id,
+      :name, :added_at, :release_date, :height, :width, :img_url, :total_tracks,
+      :spotify_id, :discogs_id,
       artists_attributes: [:id, :name, :img_url, :spotify_id, :discogs_id]
-    ) 
+    )
   end
 
   def set_album
     @album = Album.find(params[:id])
   end
 end
-
