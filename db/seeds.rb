5d6a085fd560f46f3af4b3ba4d57bde255559f90
Agathe L <lenclen.agathe@gmail.com>
2019-04-24 22:29:45 +0200

    Fix linting issues

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,39 +1,39 @@
 DatabaseCleaner.clean_with :truncation
 
-artist_0 = Artist.create({
-  name: 'artist_0', 
-  img_url: 'http://placekitten.com/300/300',
-  discogs_id: '253966',
-})
+artist0 = Artist.create(
+  name: "artist_0",
+  img_url: "http://placekitten.com/300/300",
+  discogs_id: "253966",
+)
 
-artist_1 = Artist.create({
-  name: 'artist_1', 
-  img_url: 'http://placekitten.com/300/300',
-  spotify_id: '7oqXnxR9Xg9Okhs17asfwe8S',
-})
+artist1 = Artist.create(
+  name: "artist_1",
+  img_url: "http://placekitten.com/300/300",
+  spotify_id: "7oqXnxR9Xg9Okhs17asfwe8S",
+)
 
-albums = Album.create([
+Album.create([
   {
-    name: 'album_0',
-    added_at: 'Feb, 25 2015',
-    release_date: 'June 2018',
+    name: "album_0",
+    added_at: "Feb, 25 2015",
+    release_date: "June 2018",
     total_tracks: 7,
     height: 200,
     width: 200,
-    img_url: 'http://placekitten.com/400/400',
-    discogs_id: '1443345',
-    artists: [ artist_0 ]
+    img_url: "http://placekitten.com/400/400",
+    discogs_id: "1443345",
+    artists: [ artist0 ]
   },
   {
-    name: 'album_1',
-    added_at: 'May, 25 2015',
-    release_date: 'June 2018',
+    name: "album_1",
+    added_at: "May, 25 2015",
+    release_date: "June 2018",
     total_tracks: 10,
     height: 200,
     width: 200,
-    img_url: 'http://placekitten.com/400/400',
-    discogs_id: '987762',
-    spotify_id: '2H5yYiN7sNAsxoZIg83yiD',
-    artists: [ artist_0, artist_1 ]
+    img_url: "http://placekitten.com/400/400",
+    discogs_id: "987762",
+    spotify_id: "2H5yYiN7sNAsxoZIg83yiD",
+    artists: [ artist0, artist1 ]
   }
 ])
