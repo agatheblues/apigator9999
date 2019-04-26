d2ce867db38e7725514fe3996ede64753572b57d
Agathe L <lenclen.agathe@gmail.com>
2019-04-26 23:04:26 +0200

    Add validators to artist model

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -8,9 +8,29 @@ class Artist < ApplicationRecord
 
   # validations
   validates_presence_of :name, :img_url
+  # validates_presence_of :albums, on: :create
+
+  validate :at_least_one_source_exists
+  validate :sources_are_unique, on: :create
+
+  before_destroy { albums.clear }
 
   def as_json(*)
     super(include: { albums: { except: [:created_at, :updated_at] } },
           except: [:created_at, :updated_at])
+  end
+
+  private
+
+  def at_least_one_source_exists
+    errors.add(:base, "either spotify_id or discogs_id must be present") unless
+    self[:spotify_id] || self[:discogs_id]
+  end
+
+  def sources_are_unique
+    errors.add(:base, "this spotify_id already exists in the database") if
+    self[:spotify_id] && Artist.exists?(spotify_id: self[:spotify_id])
+    errors.add(:base, "this discogs_id already exists in the database") if
+    self[:discogs_id] && Artist.exists?(discogs_id: self[:discogs_id])
   end
 end
