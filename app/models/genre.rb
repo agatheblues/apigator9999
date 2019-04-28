1877ffe6301bdfd69e3fe0d5f5d8f889ffd91f61
Agathe L <lenclen.agathe@gmail.com>
2019-04-28 16:45:07 +0200

    Add an option to genre as_json to make allow for expand / not expanded states

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -1,11 +1,20 @@
+# The Genre model
 class Genre < ApplicationRecord
   # model associations
   has_and_belongs_to_many :albums
 
   # Validations
-  validates_presence_of :name, :type
-  validates_uniqueness_of :name, scope: :type
+  validates_presence_of :name, :category
+  validates_uniqueness_of :name, scope: :category
 
   before_destroy { albums.clear }
 
+  def as_json(options = { expand: false })
+    if options[:expand]
+      super(include: { albums: { except: [:created_at, :updated_at] } },
+            except: [:created_at, :updated_at])
+    else
+      super(except: [:created_at, :updated_at])
+    end
+  end
 end
