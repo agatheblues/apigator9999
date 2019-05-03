9446dab804932569aa24d65c18e48f92bcd9383f
Agathe L <lenclen.agathe@gmail.com>
2019-05-03 21:55:04 +0200

    Fix a genre model spec (renamed type to category)

--- spec/models/genre_spec.rb
+++ spec/models/genre_spec.rb
@@ -5,6 +5,6 @@ RSpec.describe Genre, type: :model do
   it { should have_and_belong_to_many(:albums) }
 
   # Validation tests
-  it { should validate_presence_of(:type) }
+  it { should validate_presence_of(:category) }
   it { should validate_presence_of(:name) }
 end
