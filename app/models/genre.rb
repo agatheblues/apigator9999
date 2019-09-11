eb3d8eb7a672e5abd9c9a2fb8fcc5936af808a79
Agathe Lenclen <agathe@cleverfranke.com>
2019-09-11 11:54:50 +0200

    Starting fresh!

--- app/models/genre.rb
+++
@@ -1,7 +0,0 @@
-class Genre < ApplicationRecord
-  # model associations
-  has_and_belongs_to_many :albums
-  
-  # Validations
-  validates_presence_of  :name, :type
-end
--- db/migrate/20190402204102_create_join_table_albums_genres.rb
+++
@@ -1,6 +0,0 @@
-class CreateJoinTableAlbumsGenres < ActiveRecord::Migration[5.2]
-  def change
-    create_join_table :albums, :genres do |t|
-    end
-  end
-end
