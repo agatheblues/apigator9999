5d69857171632be88f2bb56a14cc2397ae75131d
Agathe Lenclen <agathe@cleverfranke.com>
2019-09-12 14:51:06 +0200

    Handle 404 and unprocessable entity errors

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,7 @@
 class ApplicationController < ActionController::API
+  include Error::ErrorHandler
+
+  rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+
 end
