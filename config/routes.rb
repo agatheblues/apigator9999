0dc01e933464ac4932c8889c5d2b35a749465863
Agathe Lenclen <agathe@cleverfranke.com>
2019-09-12 10:44:40 +0200

    Add show route for album

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get 'artists', to: 'artists#index', format: 'json'
+
+  # Albums
   get 'albums', to: 'albums#index', format: 'json'
+  get 'albums/:id', to: 'albums#show', format: 'json', as: :album
 end
