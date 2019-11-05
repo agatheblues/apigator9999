7f9793390ccb65f4cb38d4900200b381f2075f83
Agathe L <lenclen.agathe@gmail.com>
2019-11-05 23:33:05 +0100

    Add seed data for artists and albums

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,53 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+artists = Artist.create([
+  {
+    name: "Steely Dan",
+    img_url: "https://placekitten.com/200/300",
+    spotify_id: "art_spot_0"
+  },
+  {
+    name: "Radiohead",
+    img_url: "https://placekitten.com/200/300",
+    discogs_id: "art_disc_0",
+  }
+])
+
+Album.create([
+  {
+    added_at: "2018-01-01",
+    name: "Aja",
+    release_date: "1979",
+    spotify_id: "spot_0",
+    total_tracks: 12,
+    img_url: "https://placekitten.com/200/300",
+    img_height: 300,
+    img_width: 200,
+    artists: [artists[0]]
+  },
+  {
+    added_at: "2017-04-01",
+    name: "Ok Computer",
+    release_date: "2001",
+    discogs_id: "disc_0",
+    total_tracks: 22,
+    img_url: "https://placekitten.com/200/300",
+    img_height: 300,
+    img_width: 200,
+    artists: [artists[1]]
+  },
+  {
+    added_at: "2015-04-01",
+    name: "Absolution",
+    release_date: "1992",
+    discogs_id: "disc_1",
+    spotify_id: "spot_1",
+    total_tracks: 14,
+    img_url: "https://placekitten.com/200/300",
+    img_height: 300,
+    img_width: 200,
+    artists: [artists[0], artists[1]]
+  }
+])
