5e80ef3bd79e0b83c1c7c7f603fb7fd6a61751d6
Agathe L <lenclen.agathe@gmail.com>
2019-11-09 19:40:49 +0100

    Add validations to artist and album model

--- app/models/album.rb
+++ app/models/album.rb
@@ -2,6 +2,13 @@ class Album < ApplicationRecord
   # model association
   has_and_belongs_to_many :artists
 
-  # nested attributes
-  accepts_nested_attributes_for :artists
+  validates :added_at, presence: true
+  validates :name, presence: true
+  validate :has_at_least_one_id
+
+  validates_associated :artists
+
+  def has_at_least_one_id
+    errors.add(:spotify_id, "or Discogs needs a value") unless spotify_id.present? || discogs_id.present?
+  end
 end
--- app/models/artist.rb
+++ app/models/artist.rb
@@ -2,6 +2,10 @@ class Artist < ApplicationRecord
   # model association
   has_and_belongs_to_many :albums
 
-  # nested attributes
-  accepts_nested_attributes_for :albums
+  validates :name, presence: true
+  validate :has_at_least_one_id
+  
+  def has_at_least_one_id
+    errors.add(:spotify_id, "or Discogs needs a value") unless spotify_id.present? || discogs_id.present?
+  end
 end
