47cc6acb92917e67cd28931eff32ebf31b4a8d8f
Agathe L <lenclen.agathe@gmail.com>
2019-11-14 23:05:05 +0100

    Update test schemas, create tests for albums requests

--- spec/requests/albums_spec.rb
+++ spec/requests/albums_spec.rb
@@ -1,36 +1,36 @@
 require 'rails_helper'
 
-# describe "GET /albums gets all albums", :type => :request do
-#   let!(:albums) { FactoryBot.create_list(:album, 10)}
+describe "GET /albums gets all albums", :type => :request do
+  let!(:albums) { FactoryBot.create_list(:album, 10)}
 
-#   before {get '/albums'}
+  before {get '/albums'}
 
-#   it 'returns all albums' do
-#     expect(json.size).to eq(10)
-#   end
+  it 'returns all albums' do
+    expect(json.size).to eq(10)
+  end
 
-#   it 'returns status code 200' do
-#     expect(response).to have_http_status(:success)
-#   end
+  it 'returns status code 200' do
+    expect(response).to have_http_status(:success)
+  end
 
-#   it 'has the correct schema' do
-#     expect(response).to match_json_schema("album/albums")
-#   end
-# end
+  it 'has the correct schema' do
+    expect(response).to match_json_schema("album/albums")
+  end
+end
 
-# describe "GET /artists/:id gets the artist", :type => :request do
-#   let!(:id) { FactoryBot.create(:album).artists[0].id }
-#   before {get "/artists/#{id}"}
+describe "GET /albums/:id gets the album", :type => :request do
+  let!(:id) { FactoryBot.create(:album).id }
+  before {get "/albums/#{id}"}
 
-#   it 'returns the correct artist' do
-#     expect(json['id']).to eq(id)
-#   end
+  it 'returns the correct album' do
+    expect(json['id']).to eq(id)
+  end
 
-#   it 'returns status code 200' do
-#     expect(response).to have_http_status(:success)
-#   end
+  it 'returns status code 200' do
+    expect(response).to have_http_status(:success)
+  end
 
-#   it 'has the correct schema' do
-#     expect(response).to match_json_schema("artist/artist")
-#   end
-# end
+  it 'has the correct schema' do
+    expect(response).to match_json_schema("album/album_extended")
+  end
+end
