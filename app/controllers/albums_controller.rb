d00f18e4403e9226079ffdc7013760eab7d1c780
Agathe L <lenclen.agathe@gmail.com>
2019-11-24 15:04:01 +0100

    Support filtering of albums by genres or styles

--- app/controllers/albums_controller.rb
+++ app/controllers/albums_controller.rb
@@ -2,7 +2,7 @@ class AlbumsController < ApplicationController
   before_action :set_album, only: [:show, :update, :destroy]
 
   def index
-    @albums = Album.all
+    @albums = filter(Album.all)
   end
 
   def show; end
@@ -162,5 +162,29 @@ class AlbumsController < ApplicationController
     end
 
     {new: true, style: Style.create!(style_params)}
+  end
+
+  def format_params(params, name, formatter)
+    formatter.call(params[name].split(","))
+  end
+
+  def has_param(params, key)
+    params.has_key?(key) && !params[key].nil?
+  end
+
+  def filter_params
+    filter_params = {}
+    filter_params['genres'] = format_params(params, 'genres', Proc.new {|list| {:id => list}}) if has_param(params, :genres)
+    filter_params['styles'] = format_params(params, 'styles', Proc.new {|list| {:id => list}}) if has_param(params, :styles)
+    filter_params
+  end
+
+  def filter(relation)
+    return relation if filter_params.empty?
+    
+    relation = relation.joins(:genres) if filter_params.has_key?('genres')
+    relation = relation.joins(:styles) if filter_params.has_key?('styles')
+
+    relation.where(filter_params).distinct
   end
 end
