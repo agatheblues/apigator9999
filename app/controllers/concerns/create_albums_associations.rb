448b51663a6420968ebe10b2fa61620fe9e10192
Agathe L <lenclen.agathe@gmail.com>
2020-02-02 10:21:56 +0100

    Update genres and styles totals on destroy

--- app/controllers/concerns/create_albums_associations.rb
+++ app/controllers/concerns/create_albums_associations.rb
@@ -10,7 +10,7 @@ module CreateAlbumsAssociations
   def handle_genres(genres, only_new)
     genres.each do |genre|
       ActiveRecord::Base.transaction do
-        create_or_set_genre(genre, only_new) 
+        create_or_update_genre(genre, only_new) 
       end
     end 
   end
@@ -18,17 +18,21 @@ module CreateAlbumsAssociations
   def handle_styles(styles, only_new)
     styles.each do |style|
       ActiveRecord::Base.transaction do
-        create_or_set_style(style, only_new) 
+        create_or_update_style(style, only_new) 
       end
     end 
   end
 
   private
 
-  def add_totals(artist_params, current_tracks, current_albums, total_tracks)
-    artist_params['total_tracks'] = current_tracks + total_tracks
-    artist_params['total_albums'] = current_albums + 1
-    return artist_params
+  def add_total_album(params, current_albums)
+    params['total_albums'] = current_albums + 1
+    return params
+  end
+
+  def add_total_track(params, current_tracks, total_tracks)
+    params['total_tracks'] = current_tracks + total_tracks
+    return params
   end
 
   def artist_ids(artist)
@@ -47,31 +51,39 @@ module CreateAlbumsAssociations
     existing_artist = Artist.find_by(artist_ids(params))
 
     if !existing_artist.nil?
-      params = add_totals(params, existing_artist['total_tracks'], existing_artist['total_albums'], total_tracks)
+      params = add_total_album(params, existing_artist['total_albums'])
+      params = add_total_track(params, existing_artist['total_tracks'], total_tracks)
       existing_artist.update(params)
       @album.artists << existing_artist unless only_new
     else
-      params = add_totals(params, 0, 0, total_tracks)
+      params = add_total_album(params, 0)
+      params = add_total_track(params, 0, total_tracks)
       @album.artists << Artist.create!(params)
     end
   end
 
-  def create_or_set_genre(params, only_new) 
+  def create_or_update_genre(params, only_new) 
     existing_genre = Genre.find_by({name: params['name']})
 
     if !existing_genre.nil?
+      params = add_total_album(params, existing_genre['total_albums'])
+      existing_genre.update(params)
       @album.genres << existing_genre unless only_new
     else
+      params = add_total_album(params, 0)
       @album.genres << Genre.create!(params)
     end
   end
   
-  def create_or_set_style(params, only_new) 
+  def create_or_update_style(params, only_new) 
     existing_style = Style.find_by({name: params['name']})
 
-    if !existing_style.nil?
+    if !existing_style.nil? 
+      params = add_total_album(params, existing_style['total_albums'])
+      existing_style.update(params)
       @album.styles << existing_style unless only_new
     else
+      params = add_total_album(params, 0)
       @album.styles << Style.create!(params)
     end
   end
--- app/models/album.rb
+++ app/models/album.rb
@@ -5,9 +5,9 @@ class Album < ApplicationRecord
   has_and_belongs_to_many :styles
 
   before_destroy do
-    artists.each { |artist| artist.destroy if artist.albums.length == 1 }
-    genres.each { |genre| genre.destroy if genre.albums.length == 1 }
-    styles.each { |style| style.destroy if style.albums.length == 1 }
+    artists.each { |artist| handle_association(artist) }
+    genres.each { |genre| handle_association(genre) }
+    styles.each { |style| handle_association(style) }
   end
 
   validates :added_at, presence: true
@@ -18,5 +18,15 @@ class Album < ApplicationRecord
 
   def has_at_least_one_id
     errors.add(:spotify_id, "or Discogs needs a value") unless spotify_id.present? || discogs_id.present?
+  end
+
+  private
+
+  def handle_association(association)
+    if association.albums.length == 1
+      association.destroy
+    else
+      association.update({ total_albums: association.total_albums - 1 })
+    end
   end
 end
