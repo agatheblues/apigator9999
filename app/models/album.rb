e654333dacf29640fd0832e02a6f79b7ae99b76a
Agathe L <lenclen.agathe@gmail.com>
2020-02-15 17:12:36 +0100

    Remove cursed line that is incompatible with habtm associations in factorybot

--- app/models/album.rb
+++ app/models/album.rb
@@ -13,8 +13,6 @@ class Album < ApplicationRecord
   validates :added_at, presence: true
   validates :name, presence: true
   validate :has_at_least_one_id
-  
-  validates_associated :artists
 
   def has_at_least_one_id
     errors.add(:spotify_id, "or Discogs needs a value") unless spotify_id.present? || discogs_id.present?
