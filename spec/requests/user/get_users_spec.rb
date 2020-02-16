af4645043cbbaf369a1429eb0b3c6eb7e84419e6
Agathe L <lenclen.agathe@gmail.com>
2020-02-16 17:31:37 +0100

    Remove test from removed route

--- spec/requests/user/get_users_spec.rb
+++ spec/requests/user/get_users_spec.rb
@@ -2,29 +2,6 @@
 
 require 'rails_helper'
 
-describe 'GET /users gets logged in status', type: :request do
-  context 'when authenticated' do
-    before { get '/users', headers: authenticated_header }
-
-    it 'returns status code 200' do
-      expect(response).to have_http_status(:success)
-    end
-
-    it 'has the correct schema' do
-      expect(json['message']).to eq('Logged-in')
-    end
-  end
-
-  context 'when unauthenticated' do
-    before { get '/users' }
-
-    it 'returns unauthorized' do
-      expect(response).to have_http_status(:unauthorized)
-      expect(response.body).to be_empty
-    end
-  end
-end
-
 describe 'GET /users/current gets current user', type: :request do
   context 'when authenticated' do
     before { get '/users/current', headers: authenticated_header }
