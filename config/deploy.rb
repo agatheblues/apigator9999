a65cff44b1e28e38c0bdc8d61e7a1ad7543b68d2
Agathe L <lenclen.agathe@gmail.com>
2020-03-28 23:50:40 +0100

    Revert "Update Deploy - Revert me if it doesnt work"
    This reverts commit d4a7e38d5afaec90c4a60e9ad0616a0fa3e8858c.

--- config/deploy.rb
+++ config/deploy.rb
@@ -23,7 +23,7 @@ set :deploy_to, "/var/www/#{fetch :application}"
 # set :pty, true
 
 # Default value for :linked_files is []
-append :linked_files, 'config/database.yml', 'config/.rbenv-vars'
+append :linked_files, 'config/database.yml', 'config/master.key'
 
 # Default value for linked_dirs is []
 append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'public/system'
