8fff24a8cf803b0e5187eaeb376a79196646d408
Agathe L <lenclen.agathe@gmail.com>
2020-04-03 19:31:55 +0200

    Setup CORS for real

--- config/application.rb
+++ config/application.rb
@@ -36,12 +36,5 @@ module Apigator9999
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
-
-    config.middleware.insert_before 0, Rack::Cors do
-      allow do
-        origins 'localhost:8888'
-        resource '*', headers: :any, methods: [:get, :post, :options, :patch, :delete]
-      end
-    end
   end
 end
